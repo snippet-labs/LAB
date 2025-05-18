a0e3138c0cd9185f7157ee2c4a68ce4d3ae9d1f3
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-05-18 22:08:53 +0530

    project(initial): setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center text-5xl">
+        SNIPPETLABS
+      </div>
+    </div>
+  );
+};
+
+export default App;
