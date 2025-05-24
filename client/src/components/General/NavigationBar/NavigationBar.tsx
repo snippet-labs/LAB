ac16f2705b1e86fc526706c06b87ac247911bb5d
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-05-24 13:54:46 +0530

    component(navigation)(MS1): add mobile navigation icon

--- client/src/components/General/NavigationBar/NavigationBar.tsx
+++ client/src/components/General/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
+// ICONS
+import { HiMenuAlt3 } from 'react-icons/hi';
 // UTILS
 import Logo from '../../../utils/Logo/Logo';
 // DATA
@@ -10,15 +12,16 @@ const NavigationBar: React.FC = () => {
   return (
     <div className="w-full h-[9vh] BG-BLACK-PRIMARY FLEX-BETWEEN PADDING">
       <Logo className="font-style-logo text-4xl TEXT-WHITE-PRIMARY" />
-      <div className="RESPONSIVE-CENTER-BLOCK gap-4">
-        <div className="LINKS">
+      {/* Desktop Navigation Bar */}
+      <div className="hidden FLEX-CENTER-LG gap-4">
+        <div className="DESKTOP-LINKS">
           <ul className="TEXT-COLOR-PRIMARY FLEX-CENTER gap-6">
             {NAVIGATION_LINKS.map(({ name, path }, id) => (
               <li key={id} className="GRAY-300 TEXT-WHITE-PRIMARY-HOVER">
                 <NavLink
                   to={path}
                   className={({ isActive }) =>
-                    isActive ? 'LINKS-ACTIVE BG-WHITE-PRIMARY' : ''
+                    isActive ? 'DESKTOP-LINKS-ACTIVE BG-WHITE-PRIMARY' : ''
                   }
                 >
                   {name}
@@ -30,6 +33,10 @@ const NavigationBar: React.FC = () => {
         <Link to="/contact-us" className="TEXT-BLACK-PRIMARY BUTTON POINTER">
           Contract
         </Link>
+      </div>
+      {/* Mobile Navigation Bar */}
+      <div className="lg:hidden MOBILE-LINKS POINTER">
+        <HiMenuAlt3 size={20} className="TEXT-WHITE-PRIMARY" />
       </div>
     </div>
   );
