26a9223426bdb41507211880f682d5981074a3fb
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2025-05-25 20:58:29 +0530

    hook(use-route-loader): add new component for application routing; simplified routing logic with custom hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,14 @@
-import React, { lazy, Suspense, useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 // COMPONENTS
-const Layout = lazy(() => import('./components/Layout/Layout'));
-// UTILS
-import Loader from './utils/Loader/Loader';
-// ICONS
-//STORE
-
-const Content: React.FC = () => {
-  // STATES
-  const location = useLocation();
-  const [isLoading, setIsLoading] = useState<boolean>(() => false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const loadingTime = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(loadingTime);
-  }, [location.pathname]);
-  return isLoading ? <Loader /> : <Layout />;
-};
+import AppRoute from './AppRoute';
 
 const App: React.FC = () => {
   return (
-    <Suspense fallback={<Loader />}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<Content />} />
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
+    <BrowserRouter>
+      <AppRoute />
+    </BrowserRouter>
   );
 };
 
