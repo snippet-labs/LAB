532ea6a3e8de542fa0e5caf0504b8d693d394729
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2025-05-30 15:48:55 +0530

    architecture(spa): add conditional loader with routing

--- client/src/AppRoute.tsx
+++ client/src/AppRoute.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-// HOOKS
-// import useRouterLoader from './hooks/useRouterLoader';
-// UTILS
-// import Loader from './utils/Loader/Loader';
 // COMPONENTS
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
+// HOOKS
+import useRouterLoader from './hooks/useRouterLoader';
+// UTILS
+import Loader from './utils/Loader/Loader';
 
 const AppRoute: React.FC = () => {
-  // HOOKS
-  // const isLoading: boolean = useRouterLoader(1000);
+  const isLoading = useRouterLoader(1000);
 
-  // if (isLoading) return <Loader />;
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route index element={<Home />} />
-      <Route path="/contactus" element={<Contact />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="contactus" element={<Contact />} />
+      </Route>
     </Routes>
   );
 };
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-// COMPONENTS
-import NavigationBar from '../General/NavigationBar/NavigationBar';
-// UTILS
-import Loader from '../../utils/Loader/Loader';
-import useRouterLoader from '../../hooks/useRouterLoader';
-
 const Layout: React.FC = () => {
-  // HOOKS
-  const isLoading: boolean = useRouterLoader(1000);
-
-  return (
-    <div>
-      {/* Navigation */}
-      <NavigationBar />
-      {isLoading && <Loader />}
-      <main>
-        <Outlet />
-      </main>
-      {/* Footer */}
-      {/* Action */}
-    </div>
-  );
+  return <Outlet />;
 };
 
 export default Layout;
