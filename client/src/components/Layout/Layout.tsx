e9b8fd3d1198b2c164ccec238051d0bb39c2cb04
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2025-05-30 12:52:18 +0530

    architecture(spa): refine layout

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,22 +1,26 @@
-import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 // COMPONENTS
 import NavigationBar from '../General/NavigationBar/NavigationBar';
-import Footer from '../General/Footer/Footer';
 // UTILS
 import Loader from '../../utils/Loader/Loader';
+import useRouterLoader from '../../hooks/useRouterLoader';
 
 const Layout: React.FC = () => {
+  // HOOKS
+  const isLoading: boolean = useRouterLoader(1000);
+
   return (
     <div>
+      {/* Navigation */}
       <NavigationBar />
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<></>} />
-        </Routes>
-      </Suspense>
-      <Footer />
+      {isLoading && <Loader />}
+      <main>
+        <Outlet />
+      </main>
+      {/* Footer */}
+      {/* Action */}
     </div>
   );
 };
