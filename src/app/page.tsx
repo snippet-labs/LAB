d3ae53af3c2ae7e17c5780bbb5ca8ab189c9911a
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-07-30 20:19:06 +0530

    component(button): add reuseable button component in header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,22 @@ const HomePage = () => {
         <Background
           title="SNIPPETLABS"
           description="From our labs for your dreams"
+          isButtonActive={true}
+          buttonDescription="Build your next dream product with us"
         />
       </section>
       <section id="knowus">
-        <Background title="KNOW OUR COMPANY" />
+        <Background
+          title="KNOW OUR COMPANY"
+          description="We're an emerging product development studio on a mission to turn ideas into intuitive, delightful, and scalable digital experiences. Whether you're a startup with a sketch or a brand with a vision, we dive deep with love, empathy, and fun at the core of everything we create. Our team blends craftsmanship with compassion, ensuring every pixel, line of code, and interaction feels personal and purposeful. We believe the best products come from close collaboration, honest conversations, and a little bit of magic"
+          isButtonActive={false}
+        />
       </section>
       <section id="technology">
-        <Background title="TECHNOLOGY STACK" />
+        <Background title="TECHNOLOGY STACK" isButtonActive={false} />
       </section>
       <section id="motto">
-        <Background title="OUR MOTTO" />
+        <Background title="OUR MOTTO" isButtonActive={false} />
       </section>
     </>
   );
