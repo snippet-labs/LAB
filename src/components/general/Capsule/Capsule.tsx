d0ba75d7cc9acb6a13e51efcb74bb3f86ae533b8
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-07-31 00:31:57 +0530

    page(404): steamlined module imports

--- src/components/general/Capsule/Capsule.tsx
+++ src/components/general/Capsule/Capsule.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-// Types
+// Modules
 import { CapsuleTypes } from './Capsule.types';
 
-// Button
 const Capsule = ({ title }: CapsuleTypes) => {
   return (
     <button className="text-white text-sm md:text-md lg:text-lg border-1 border-purple-500 backdrop-blur-3xl bg-purple-500/10 rounded-full px-5 md:px-10 lg:px-15 py-2 animate-pulse z-20">
