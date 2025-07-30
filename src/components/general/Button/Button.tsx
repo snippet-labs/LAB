1ae7f7bd3bc2600639aa88138d8f422141f615d3
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-07-30 20:33:28 +0530

    component(button): add frosted-backgroud

--- src/components/general/Button/Button.tsx
+++ src/components/general/Button/Button.tsx
@@ -6,7 +6,7 @@ import { ButtonTypes } from './Button.types';
 // Button
 const Button = ({ title }: ButtonTypes) => {
   return (
-    <div className="text-white text-sm md:text-md lg:text-lg border-1 border-purple-500 rounded-full px-5 md:px-10 lg:px-15 py-2 animate-pulse z-20">
+    <div className="text-white text-sm md:text-md lg:text-lg border-1 border-purple-500 bg-purple-500/10 rounded-full px-5 md:px-10 lg:px-15 py-2 animate-pulse z-20">
       {title}
     </div>
   );
