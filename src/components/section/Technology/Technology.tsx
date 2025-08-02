deaa9739e9bc119277cd9ab4df74ac0a42b04fd7
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-08-02 18:01:50 +0530

    section(technology): add next-image for optimization

--- src/components/section/Technology/Technology.tsx
+++ src/components/section/Technology/Technology.tsx
@@ -6,6 +6,7 @@ import { monotonFont } from '@/helpers/font';
 import { motion } from 'motion/react';
 import { useSectionInView } from '@/hooks/useSectionInView';
 import { stackData } from '@/data/stackData';
+import Image from 'next/image';
 
 const Technology = () => {
   const { ref, isSectionInViewPort } = useSectionInView();
@@ -25,7 +26,7 @@ const Technology = () => {
             transition={{ duration: 1, ease: 'easeOut' }}
           >
             <h1
-              className={`text-5xl md:text-7xl lg:text-8xl bg-clip-text text-transparent  bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 logo-gradient-shift-animation text-left transition-all mb-4 ${monotonFont.className}`}
+              className={`text-5xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 logo-gradient-shift-animation text-left transition-all mb-4 ${monotonFont.className}`}
             >
               OUR STACK
             </h1>
@@ -35,7 +36,7 @@ const Technology = () => {
             animate={isSectionInViewPort ? { opacity: 1, y: 0 } : {}}
             transition={{ delay: 0.3, duration: 1, ease: 'easeOut' }}
           >
-            <p className="text-lg md:text-xl text-justify leading-relaxed  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600">
+            <p className="text-lg md:text-xl text-justify leading-relaxed bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600">
               Our weapons of choice, our technology stack that not only give us
               en edge to build something grand efforlessly but make our dream
               projects happy and delighted.
@@ -45,20 +46,24 @@ const Technology = () => {
             initial={{ opacity: 0, y: 30 }}
             animate={isSectionInViewPort ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.6, ease: 'easeOut' }}
+            className="max-w-4xl"
           >
-            <div className="flex overflow-hidden border p-1 mt-15 EDGEGRADIENT">
+            <div className="flex overflow-hidden border mt-15  EDGEGRADIENT">
               <motion.div
                 initial={{ x: 0 }}
                 animate={{ x: '-100%' }}
                 transition={{ duration: 35, repeat: Infinity, ease: 'linear' }}
-                className="flex flex-shrink-0 gap-1 EDGEGRADIENT"
+                className="flex flex-shrink-0 gap-1"
               >
                 {stackData.map((image, index) => {
                   return (
-                    <img
+                    <Image
                       src={image}
+                      alt=""
+                      width={70}
+                      height={70}
                       key={index}
-                      className="w-15 h-15 md:w-20 lg:w-25 md:h-20 lg:h-25  group bg-white/5 backdrop-blur-md border border-neutral-800 rounded-2xl shadow-xl"
+                      className="group bg-white/5 backdrop-blur-md border border-neutral-800 rounded-2xl shadow-xl"
                     />
                   );
                 })}
@@ -67,14 +72,17 @@ const Technology = () => {
                 initial={{ x: 0 }}
                 animate={{ x: '-100%' }}
                 transition={{ duration: 35, repeat: Infinity, ease: 'linear' }}
-                className="flex flex-shrink-0 gap-1 EDGEGRADIENT"
+                className="flex flex-shrink-0 gap-1"
               >
                 {stackData.map((image, index) => {
                   return (
-                    <img
+                    <Image
                       src={image}
+                      alt={''}
+                      width={70}
+                      height={70}
                       key={index}
-                      className="w-15 h-15 md:w-20 lg:w-25 md:h-20 lg:h-25 group bg-white/5 backdrop-blur-md border border-neutral-800 rounded-2xl shadow-xl"
+                      className="group bg-white/5 backdrop-blur-md border border-neutral-800 rounded-2xl shadow-xl"
                     />
                   );
                 })}
